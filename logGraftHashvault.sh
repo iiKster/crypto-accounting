70e2e4394a2c14537c6c73e400f088dfd6e48437
iiKster <janne.raitanen@gmail.com>
2018-07-11 14:18:04 +0300

    Create logGraftHashvault.sh

---
+++ logGraftHashvault.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Graft from Hashwault
+# Author: iiKster,  July 11 2018
+# Write a CSV file of your payments from Hashvault when mining graft.
+# The CSV file contains: Date,  Payout in GRFT, GRFT/BTC value
+# Be sure to put in your GRFT address and path to the datafile to get some results
+# Data is fetched from Hashvault and Coingecko JOSN API
+
+
+oldTime=0
+int=0
+# start loop
+while [  $int -lt 500 ]; do
+
+        # Get the values from Hashvault / Coingecko json api
+        # and assign them to variables
+        lastTime=$(curl -s https://graft.hashvault.pro/api/miner/GBSMnCdN72RfHYRfBfN1MZNac5tb7TX53dkv666DLGtk64xKtZg4gXUgwik7ConThe9tpEtL8zA3xb6R84BERGht9oGXFxn/payments?page | jq -r '.[0]$
+        lastPay=$(curl -s https://graft.hashvault.pro/api/miner/GBSMnCdN72RfHYRfBfN1MZNac5tb7TX53dkv666DLGtk64xKtZg4gXUgwik7ConThe9tpEtL8zA3xb6R84BERGht9oGXFxn/payments?page | jq -r '.[0] $
+        grftBtcValue=$(curl -s https://api.coingecko.com/api/v3/coins/graft-blockchain | jq -r '.market_data .current_price .btc')
+        btcEurValue=$(curl -s https://api.coingecko.com/api/v3/exchange_rates | jq -r '.rates .eur .value')
+
+        # Get current date
+        date=`date +%d.%m.%Y`
+
+        payment(){ awk "BEGIN { print "$*" }"; }
+
+# Format the payout to coins
+        div=10000000000
+        payment=$(awk 'BEGIN {print ('$lastPay'/'$div')}')
+
+
+        #Graft value to BTC is so low the JSON output was 2.234e-6, let's AWK it to a float with 15 decimals
+        grftBtcValueFloat=$(awk -v grftBtcValue="$grftBtcValue" 'BEGIN { printf("%.16f\n", grftBtcValue) }' </dev/null)
+
+        if [ "$lastTime" != "" ]
+        then
+
+                if [ "$lastTime" != "$oldTime" ]
+                then
+                        #Print the stuff to a file
+                        # If you run the script with @reboot from crontab please specify the full path to the file
+                        echo -e $date","$payment","$grftBtcValueFloat","$btcEurValue >> /home/pi/GRFTLog.txt
+                        oldTime=$lastTime
+                fi
+        fi
+        sleep 1m
+
+done
